d3df115331658cb963db1c1d9dcccfe193f32489
cmr.evan.duru <evan.duru@cmrsurgical.com>
2022-11-13 15:15:22 +0000

    commit

---
+++ react-practice/src/App.js
@@ -0,0 +1,11 @@
+
+import {Animal} from './Components/Box/box.jsx'
+function App() {
+  return (
+    <>
+    <Animal name='Cat' color='black' />
+    </>
+  );
+}
+
+export default App;
---
+++ react-practice/src/Components/Box/box.jsx
@@ -0,0 +1,13 @@
+
+
+export const Animal = ({name, color}) => 
+{
+    console.log(name, color);    
+    return(
+        <>
+            <p>The animal is a:{name} and thge color is: {color}</p>
+            
+        </>
+
+    )
+}

f08e1b11dc0249c2c593d8cd8102c343f27bdd3e
cmr.evan.duru <evan.duru@cmrsurgical.com>
2022-11-13 14:45:15 +0000

    react route outlet

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,12 +1,29 @@
-import { Routes, Route} from 'react-router-dom'
+import { Routes, Route, Outlet} from 'react-router-dom'
 //Components
 import Home from './components/routes/home.component'
 ///SCSS
 
+const Navigation = () => 
+{
+	return(
+		<div>
+			<div>
+				<h1>This is the navigation bar</h1>
+			</div>
+			{/* to be able to persist this component on the browser to a specified path we use the ->> <Outlet/> */}
+			<Outlet/>
+		</div>
+	)
+}
 
 const  App = () => {
   	return(
-		<Routes><Route path='/home' element={ <Home /> }> </Route></Routes>
+		<Routes>
+			{/* nav and home page persisted when path = '/' */}
+			<Route path='/' element={ <Navigation /> }>
+				<Route index element={<Home/>}/>
+			</Route>
+		</Routes>
   	);
 }
 

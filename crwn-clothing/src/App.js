3b7dd13ac842211e25d72b8e873c3139f644226f
cmr.evan.duru <evan.duru@cmrsurgical.com>
2022-11-20 13:29:29 +0000

    router link

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,30 +1,23 @@
-import { Routes, Route, Outlet} from 'react-router-dom'
-//Components
-import Home from './components/routes/home.component'
-///SCSS
+import { Routes, Route } from 'react-router-dom';
 
-const Navigation = () => 
-{
-	return(
-		<div>
-			<div>
-				<h1>This is the navigation bar</h1>
-			</div>
-			{/* to be able to persist this component on the browser to a specified path we use the ->> <Outlet/> */}
-			<Outlet/>
-		</div>
-	)
-}
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
-const  App = () => {
-  	return(
-		<Routes>
-			{/* nav and home page persisted when path = '/' */}
-			<Route path='/' element={ <Navigation /> }>
-				<Route index element={<Home/>}/>
-			</Route>
-		</Routes>
-  	);
-}
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;

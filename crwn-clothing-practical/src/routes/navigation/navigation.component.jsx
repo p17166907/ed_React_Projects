a525f76babd073feb76daf16bcf85f268dd21107
cmr.evan.duru <evan.duru@cmrsurgical.com>
2022-12-06 13:55:12 +0000

    practice component navigation route

---
+++ crwn-clothing-practical/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,20 @@
+import {Fragment} from 'react'
+import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './navigation.style.scss';
+
+
+export const Navigation = () =>
+{
+    return(
+    <Fragment>
+		<div className="navigation">
+			<Link className='logo-container' to='/'> {<CrwnLogo/>}</Link>
+			<div className="nav-links-container">
+				<Link className='nav-link' to='/shop'>SHOP</Link>
+				<Link className='nav-link' to='/sign-in'>SIGN IN</Link>
+			</div>
+		</div>
+		<Outlet/>
+	</Fragment>)
+}
